cfe4710e9b8ff343ac742548ef99929af0211056
dmatmyown <dm.kim@myown.kr>
2022-12-01 13:27:53 +0900

    add: initial setups
    - install tailwindcss
    - config according to styleguide
    - add icons and images

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,23 +2,39 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `chanwoo-gatsby-221117`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `chanwoo-gatsby-221117`,
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
-};
+    plugins: [
+        "gatsby-plugin-postcss",
+        "gatsby-plugin-image",
+        "gatsby-plugin-sitemap",
+        "gatsby-transformer-remark",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "pages",
+                path: "./src/pages/",
+            },
+            __key: "pages",
+        },
+        {    
+            resolve: "gatsby-plugin-react-svg",
+            options: {
+                rule: {
+                    include: /images/ // See below to configure properly
+                }
+            }
+        }
+    ],
+}
